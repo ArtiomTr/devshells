7580bf58d7a52f0e2c0f4eaf0d7e7f4b0597575a
Artiom Tretjakovas <hi@sirse.dev>
2026-05-07 22:26:49 +0300

    update rust devshell

--- lib/mk-rust-shell.nix
+++ lib/mk-rust-shell.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 { toolchainVersion, extraPackages ? [ ] }:
 let
-  rustToolchain =
+  baseRustToolchain =
     if toolchainVersion == "stable" then
       pkgs.rust-bin.stable.latest.default
     else if toolchainVersion == "beta" then
@@ -10,10 +10,16 @@ let
       pkgs.rust-bin.nightly.latest.default
     else
       pkgs.rust-bin.stable.${toolchainVersion}.default;
+
+  rustToolchain = baseRustToolchain.override {
+    extensions = [ "rust-src" ];
+  };
 in
 pkgs.mkShell {
   packages = [
     rustToolchain
     pkgs.pkg-config
   ] ++ map (name: pkgs.${name}) extraPackages;
+
+  RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
 }
